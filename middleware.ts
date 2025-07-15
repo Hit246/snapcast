6eb2a1ea02be36f15303e570634606450bdf5311
Hitarth Chauhan <hitarth0236@gmail.com>
2025-07-15 18:32:31 +0530

    last

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
-import aj, { createMiddleware, shield } from "./lib/arcjet";
 
 export async function middleware(request: NextRequest) {
   const session = await auth.api.getSession({
@@ -12,14 +11,6 @@ export async function middleware(request: NextRequest) {
   }
   return NextResponse.next();
 }
-const validate = aj
-  .withRule(
-    shield({
-      mode: "LIVE",
-    })
-  );
-
-export default createMiddleware(validate);
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sign-in|assets).*)"],
