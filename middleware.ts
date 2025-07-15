3e7c4f97e55f2f59a8d693e7ba0293394a973989
Hitarth Chauhan <hitarth0236@gmail.com>
2025-07-15 18:07:36 +0530

    error solved

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
-import { auth } from "./lib/auth";
 import { headers } from "next/headers";
-import aj from "./lib/arcjet";
-import { createMiddleware, detectBot, shield } from "@arcjet/next";
+import { auth } from "@/lib/auth";
+import aj, { createMiddleware, detectBot, shield } from "./lib/arcjet";
 
-export async function middleware(request: NextRequest, response: NextResponse) {
-    const session = await auth.api.getSession({
-        headers: await headers()
-    })
+export async function middleware(request: NextRequest) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-    if(!session) {
-        return NextResponse.redirect(new URL('/sign-in', request.url))
-    }
+  if (!session) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
 
-    return NextResponse.next();
+  return NextResponse.next();
 }
-
 const validate = aj
-            .withRule(shield({mode: 'LIVE'}))
-            .withRule(detectBot({mode: 'LIVE', allow: ['CATEGORY:SEARCH_ENGINE', 'GOOGLE_CRAWLER']}))
+  .withRule(
+    shield({
+      mode: "LIVE",
+    })
+  )
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "G00G1E_CRAWLER"], // allow other bots if you want to.
+    })
+  );
 
 export default createMiddleware(validate);
 
 export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sign-in|assets).*)"]
-}
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sign-in|assets).*)"],
+};
